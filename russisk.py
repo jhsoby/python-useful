378b4af3d5f0d748837f41190d2a99c52fec78c2
Jon Harald Søby <jhsoby@gmail.com>
2016-05-19 11:23:24 +0200

    Rewriting russisk.py to do both Norwegian and English transcription

--- russisk.py
+++ russisk.py
@@ -3,8 +3,10 @@
 import pyperclip
 import time
 import string
+import re
+import os
 
-russisk = {
+ru_no = {
 	u"а": "a",
 	u"б": "b",
 	u"в": "v",
@@ -42,41 +44,120 @@ russisk = {
 	u"-": "-",
 	u",": ",",
 }
+ru_en = {
+	u"а": "a",
+	u"б": "b",
+	u"в": "v",
+	u"г": "g",
+	u"д": "d",
+	u"е": ["ye", "e"], # "e" vanligvis, "ye" først i ord eller etter vokal, Ь eller Ъ
+	u"ё": ["yo", "o"], # "yo" vanligvis, "o" etter y
+	u"ж": "zh",
+	u"з": "z",
+	u"и": "i",
+	u"й": "y",
+	u"к": "k",
+	u"л": "l",
+	u"м": "m",
+	u"н": "n",
+	u"о": "o",
+	u"п": "p",
+	u"р": "r",
+	u"с": "s",
+	u"т": "t",
+	u"у": "u",
+	u"ф": "f",
+	u"х": "kh",
+	u"ц": "ts",
+	u"ч": "ch",
+	u"ш": "sh",
+	u"щ": "shch",
+	u"ъ": "",
+	u"ы": "y",
+	u"ь": "",
+	u"э": "e",
+	u"ю": ["yu", "u"], # "yu" vanligvis, "u" etter y
+	u"я": ["ya", "a"], # "ya" vanligvis, "a" etter y
+	u" ": " ",
+	u"-": "-",
+	u",": ",",
+}
 
-#innputt = raw_input("Russisk ord: ").decode('utf-8').lower()
+def kommafiks(navn):
+	if string.find(navn, ",") != -1:
+		return navn[string.find(navn, ",")+2:] + " " + navn[:string.find(navn, ",")]
+	else:
+		return navn
 
-def transkripsjon(russiskord):
-	norsk = ""
-	russ = ""
+def nortrans(russiskord):
+	nor = ""
+	rus = ""
 	for bokstav in russiskord:
-		russ += bokstav
-		if isinstance(russisk[bokstav], list):
+		rus += bokstav
+		if isinstance(ru_no[bokstav], list):
 			if bokstav == u"е":
-				if (" " + russ)[-2] in u" ьъ":
-					if (" " + norsk)[-1] in "sz":
-						norsk += russisk[bokstav][1]
+				if (" " + rus)[-2] in u" ьъ":
+					if (" " + nor)[-1] in "sz":
+						nor += ru_no[bokstav][1]
 					else:
-						norsk += russisk[bokstav][0]
-				elif (" " + norsk)[-1] in "aeiouy":
-					norsk += russisk[bokstav][0]
+						nor += ru_no[bokstav][0]
+				elif (" " + nor)[-1] in "aeiouy":
+					nor += ru_no[bokstav][0]
 				else:
-					norsk += russisk[bokstav][2]
-			elif (" " + norsk)[-1] == "j":
-				norsk += russisk[bokstav][2]
-			elif (" " + norsk)[-1] in "sz":
-				norsk += russisk[bokstav][1]
+					nor += ru_no[bokstav][2]
+			elif (" " + nor)[-1] == "j":
+				nor += ru_no[bokstav][2]
+			elif (" " + nor)[-1] in "sz":
+				nor += ru_no[bokstav][1]
 			else:
-				norsk += russisk[bokstav][0]
+				nor += ru_no[bokstav][0]
 		else:
-			norsk += russisk[bokstav]
-	resultat = string.capwords(norsk)
-	if not string.find(resultat, ",") == -1:
-		resultat = resultat[string.find(resultat, ",")+2:] + " " + resultat[:string.find(resultat, ",")]
-	pyperclip.copy(resultat)
-	print "Klar for innliming: " + resultat
+			nor += ru_no[bokstav]
+	nor = string.capwords(nor)
+	nor = kommafiks(nor)
+	pyperclip.copy(nor)
+	print "Klar for innliming (norsk): " + nor
 
-while True:
-	try:
-		transkripsjon(pyperclip.paste().decode('utf-8').lower())
-	except KeyError:
-		time.sleep(0.1)
+def engtrans(russiskord):
+	orig = re.sub(u"[иы]й(\\s|\\-|$)", u"й\\1", kommafiks(russiskord))
+	eng = ""
+	rus = ""
+	for bokstav in orig:
+		rus += bokstav
+		if isinstance(ru_en[bokstav], list):
+			if bokstav == u"е":
+				if ((" " + rus)[-2] in u" ьъ") or ((" " + eng)[-1] in "aeiouy"):
+					eng += ru_en[bokstav][0]
+				else:
+					eng += ru_en[bokstav][1]
+			elif (" " + eng)[-1] == "y":
+				eng += ru_en[bokstav][1]
+			else:
+				eng += ru_en[bokstav][0]
+		else:
+			eng += ru_en[bokstav]
+	eng = string.capwords(eng)
+	pyperclip.copy(eng)
+	print "Klar for innliming (engelsk): " + eng
+	os.system('paplay /usr/share/sounds/ubuntu/notifications/Blip.ogg')
+
+def motor():
+	orig = ""
+	while True:
+		liminn = pyperclip.paste().decode('utf-8').lower()
+		if orig == liminn:
+			try:
+				engtrans(liminn)
+			except KeyError:
+				time.sleep(0.1)
+		else:
+			try:
+				nortrans(liminn)
+			except KeyError:
+				time.sleep(0.1)
+		if re.search("[aeiouy]", liminn):
+			time.sleep(0.1)
+		else:
+			orig = liminn
+
+motor()
